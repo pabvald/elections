72f8b2018755ea79118dcf2ae56f7f17103abd54
pabvald <pablo.valdunciel@alumnos.uva.es>
2020-01-12 20:51:04 +0100

    json schema updated

--- main/static/scripts/validators.js
+++ main/static/scripts/validators.js
@@ -1,6 +1,14 @@
 
 
+let DISTRICT_NAME_PATTERN = /^([a-zA-ZÀ-ÿ\u00f1\u00d1]){1,120}$/;
+let CANDIDATURE_NAME_PATTERN = /^([a-zA-ZÀ-ÿ\u00f1\u00d1]){1}([\wÀ-ÿ\u00f1\u00d1-]){0,69}$/;
+let CANDIDATURE_ABRV_NAME_PATTERN = /^([a-zA-ZÀ-ÿ\u00f1\u00d1]){1}([\wÀ-ÿ\u00f1\u00d1-]){0,5}$/;
 
 function validateDistrict(district) {
 
+}
+
+
+function validateCandidature(candidature) {
+    
 }
